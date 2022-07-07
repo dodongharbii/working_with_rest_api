157a6d6a0621481ffe8dbd519e5926b491d9256d
harbii <vonnehrvy98@gmail.com>
2022-07-07 18:37:32 +0800

    Created notelist page

---
+++ lib/views/note_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatelessWidget {
+  const NoteList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('List of Notes')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Container(),
+    );
+  }
+}
