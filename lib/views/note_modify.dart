54b8baee793e86a5d11a938b2aa57f4b707104df
harbii <vonnehrvy98@gmail.com>
2022-07-08 22:09:52 +0800

    used getNote method on noteModify to get note content and display it

--- lib/views/note_modify.dart
+++ lib/views/note_modify.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:tuso_working_with_rest_api/models/note.dart';
+import 'package:tuso_working_with_rest_api/services/notes_service.dart';
 
-class NoteModify extends StatelessWidget {
+class NoteModify extends StatefulWidget {
   //const NoteModify({ Key? key }) : super(key: key);
 
   String? noteID;
-  bool get isEditing => noteID != null;
-
   NoteModify({this.noteID});
+
+  @override
+  State<NoteModify> createState() => _NoteModifyState();
+}
+
+class _NoteModifyState extends State<NoteModify> {
+  bool get isEditing => widget.noteID != null;
+
+  NotesService get notesService => GetIt.I<NotesService>();
+
+  String? errorMessage;
+  Note? note;
+
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _contentController = TextEditingController();
+
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    notesService.getNote(widget.noteID!).then((response) {
+      setState(() {
+        _isLoading = false;
+      });
+
+      if (response.error) {
+        errorMessage = response.errorMessage ?? 'An error occured';
+      }
+      note = response.data;
+      _titleController.text = note!.noteTitle;
+      _contentController.text = note!.noteContent;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +54,15 @@ class NoteModify extends StatelessWidget {
       appBar: AppBar(title: Text(isEditing ? 'Edit Note' : 'Create Note')),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
+        child: _isLoading ? Center(child: CircularProgressIndicator()) : Column(
           children: <Widget>[
             TextField(
+              controller: _titleController,
               decoration: InputDecoration(hintText: "Note title"),
             ),
             Container(height: 8),
             TextField(
+              controller: _contentController,
               decoration: InputDecoration(hintText: "Note content"),
             ),
             Container(height: 16),
