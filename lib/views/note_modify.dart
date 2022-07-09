77f03592cbc4efa7dc18100bd0c7596b5e15608c
harbii <vonnehrvy98@gmail.com>
2022-07-09 15:10:38 +0800

    implemented onPressed of submit btn (updating notes)

--- lib/views/note_modify.dart
+++ lib/views/note_modify.dart
@@ -83,13 +83,48 @@ class _NoteModifyState extends State<NoteModify> {
                       ),
                       onPressed: () async {
                         if (isEditing) {
-                          //update note
+                          setState(() {
+                            _isLoading = true;
+                          });
+
+                          final note = NoteManipulation(
+                              noteTitle: _titleController.text,
+                              noteContent: _contentController.text);
+                          final result = await notesService.updateNote(widget.noteID! ,note);
+
+                          setState(() {
+                            _isLoading = false;
+                          });
+
+                          final title = 'Done';
+                          final text = result.error
+                              ? (result.errorMessage ?? 'An error occured')
+                              : 'Your note was updated';
+
+                          showDialog(
+                              context: context,
+                              builder: (_) => AlertDialog(
+                                    title: Text(title),
+                                    content: Text(text),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        child: Text('Ok'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  )).then((data) {
+                            if (result.data!) {
+                              Navigator.of(context).pop();
+                            }
+                          });
                         } else {
                           setState(() {
                             _isLoading = true;
                           });
 
-                          final note = NoteInsert(
+                          final note = NoteManipulation(
                               noteTitle: _titleController.text,
                               noteContent: _contentController.text);
                           final result = await notesService.createNote(note);
